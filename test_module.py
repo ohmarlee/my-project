3f280b9ebf33f8f76e59a542b3abe5063722318c
ohmarlee <147035241+ohmarlee@users.noreply.github.com>
2023-10-05 02:22:42 -0700

    Add files via upload

--- test_module.py
+++ test_module.py
@@ -1,43 +1,97 @@
 import unittest
-from arithmetic_arranger import arithmetic_arranger
+import budget
+from budget import create_spend_chart
 
 
-# the test case
 class UnitTests(unittest.TestCase):
-    def test_arrangement(self):
-        actual = arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"])
-        expected = "    3      3801      45      123\n+ 855    -    2    + 43    +  49\n-----    ------    ----    -----"
-        self.assertEqual(actual, expected, 'Expected different output when calling "arithmetic_arranger()" with ["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]')
+    def setUp(self):
+        self.food = budget.Category("Food")
+        self.entertainment = budget.Category("Entertainment")
+        self.business = budget.Category("Business")
 
-        actual = arithmetic_arranger(["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"])
-        expected = "  11      3801      1      123         1\n+  4    - 2999    + 2    +  49    - 9380\n----    ------    ---    -----    ------"
-        self.assertEqual(actual, expected, 'Expected different output when calling "arithmetic_arranger()" with ["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"]')
+    def test_deposit(self):
+        self.food.deposit(900, "deposit")
+        actual = self.food.ledger[0]
+        expected = {"amount": 900, "description": "deposit"}
+        self.assertEqual(actual, expected, 'Expected `deposit` method to create a specific object in the ledger instance variable.')
 
-    def test_too_many_problems(self):
-        actual = arithmetic_arranger(["44 + 815", "909 - 2", "45 + 43", "123 + 49", "888 + 40", "653 + 87"])
-        expected = "Error: Too many problems."
-        self.assertEqual(actual, expected, 'Expected calling "arithmetic_arranger()" with more than five problems to return "Error: Too many problems."')
+    def test_deposit_no_description(self):
+        self.food.deposit(45.56)
+        actual = self.food.ledger[0]
+        expected = {"amount": 45.56, "description": ""}
+        self.assertEqual(actual, expected, 'Expected calling `deposit` method with no description to create a blank description.')
 
-    def test_incorrect_operator(self):
-        actual = arithmetic_arranger(["3 / 855", "3801 - 2", "45 + 43", "123 + 49"])
-        expected = "Error: Operator must be '+' or '-'."
-        self.assertEqual(actual, expected, '''Expected calling "arithmetic_arranger()" with a problem that uses the "/" operator to return "Error: Operator must be '+' or '-'."''')
-        
-    def test_too_many_digits(self):
-        actual = arithmetic_arranger(["24 + 85215", "3801 - 2", "45 + 43", "123 + 49"])
-        expected = "Error: Numbers cannot be more than four digits."
-        self.assertEqual(actual, expected, 'Expected calling "arithmetic_arranger()" with a problem that has a number over 4 digits long to return "Error: Numbers cannot be more than four digits."')
+    def test_withdraw(self):
+        self.food.deposit(900, "deposit")
+        self.food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
+        actual = self.food.ledger[1]
+        expected = {"amount": -45.67, "description": "milk, cereal, eggs, bacon, bread"}
+        self.assertEqual(actual, expected, 'Expected `withdraw` method to create a specific object in the ledger instance variable.')
 
-    def test_only_digits(self):
-        actual = arithmetic_arranger(["98 + 3g5", "3801 - 2", "45 + 43", "123 + 49"])
-        expected = "Error: Numbers must only contain digits."
-        self.assertEqual(actual, expected, 'Expected calling "arithmetic_arranger()" with a problem that contains a letter character in the number to return "Error: Numbers must only contain digits."')
+    def test_withdraw_no_description(self):
+        self.food.deposit(900, "deposit")
+        good_withdraw = self.food.withdraw(45.67)
+        actual = self.food.ledger[1]
+        expected = {"amount": -45.67, "description": ""}
+        self.assertEqual(actual, expected, 'Expected `withdraw` method with no description to create a blank description.')
+        self.assertEqual(good_withdraw, True, 'Expected `transfer` method to return `True`.')
 
-    def test_solutions(self):
-        actual = arithmetic_arranger(["32 - 698", "1 - 3801", "45 + 43", "123 + 49"], True)
-        expected = "   32         1      45      123\n- 698    - 3801    + 43    +  49\n-----    ------    ----    -----\n -666     -3800      88      172"
-        self.assertEqual(actual, expected, 'Expected solutions to be correctly displayed in output when calling "arithmetic_arranger()" with arithmetic problems and a second argument of `True`.')
+    def test_get_balance(self):
+        self.food.deposit(900, "deposit")
+        self.food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
+        actual = self.food.get_balance()
+        expected = 854.33
+        self.assertEqual(actual, expected, 'Expected balance to be 854.33')
 
+    def test_transfer(self):
+        self.food.deposit(900, "deposit")
+        self.food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
+        good_transfer = self.food.transfer(20, self.entertainment)
+        actual = self.food.ledger[2]
+        expected = {"amount": -20, "description": "Transfer to Entertainment"}
+        self.assertEqual(actual, expected, 'Expected `transfer` method to create a specific ledger item in food object.')
+        self.assertEqual(good_transfer, True, 'Expected `transfer` method to return `True`.')
+        actual = self.entertainment.ledger[0]
+        expected = {"amount": 20, "description": "Transfer from Food"}
+        self.assertEqual(actual, expected, 'Expected `transfer` method to create a specific ledger item in entertainment object.')
+
+    def test_check_funds(self):
+        self.food.deposit(10, "deposit")
+        actual = self.food.check_funds(20)
+        expected = False
+        self.assertEqual(actual, expected, 'Expected `check_funds` method to be False')
+        actual = self.food.check_funds(10)
+        expected = True
+        self.assertEqual(actual, expected, 'Expected `check_funds` method to be True')
+
+    def test_withdraw_no_funds(self):
+        self.food.deposit(100, "deposit")
+        good_withdraw = self.food.withdraw(100.10)
+        self.assertEqual(good_withdraw, False, 'Expected `withdraw` method to return `False`.')
+
+    def test_transfer_no_funds(self):
+        self.food.deposit(100, "deposit")
+        good_transfer = self.food.transfer(200, self.entertainment)
+        self.assertEqual(good_transfer, False, 'Expected `transfer` method to return `False`.')
+
+    def test_to_string(self):
+        self.food.deposit(900, "deposit")
+        self.food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
+        self.food.transfer(20, self.entertainment)
+        actual = str(self.food)
+        expected = f"*************Food*************\ndeposit                 900.00\nmilk, cereal, eggs, bac -45.67\nTransfer to Entertainme -20.00\nTotal: 834.33"
+        self.assertEqual(actual, expected, 'Expected different string representation of object.')
+
+    def test_create_spend_chart(self):
+        self.food.deposit(900, "deposit")
+        self.entertainment.deposit(900, "deposit")
+        self.business.deposit(900, "deposit")
+        self.food.withdraw(105.55)
+        self.entertainment.withdraw(33.40)
+        self.business.withdraw(10.99)
+        actual = create_spend_chart([self.business, self.food, self.entertainment])
+        expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+        self.assertEqual(actual, expected, 'Expected different chart representation. Check that all spacing is exact.')
 
 if __name__ == "__main__":
     unittest.main()
